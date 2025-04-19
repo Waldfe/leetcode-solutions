aee49d0db165a19e0ffb297af9f59d7cc26330dc
Felipe Kerwald <kerwald.s@gmail.com>
2025-04-19 09:25:34 -0300

    adicionar solução para o problema majority element

---
+++ 0169-majority-element/main.c
@@ -0,0 +1,22 @@
+// Leetcode Problem #169 - Majority Element
+// https://leetcode.com/problems/majority-element/
+
+#include<stdio.h>
+
+int majorityElement( int* nums, int numsSize ) {
+
+    int possivel = 0;
+    int count = 0;
+
+    for( int i=0; i<numsSize; i++ ){
+        if( count == 0 ){
+            possivel = nums[i];
+        }
+        if( nums[i] == possivel ){
+            count++;
+        }else{
+            count--;
+        }
+    }
+    return possivel;
+}
